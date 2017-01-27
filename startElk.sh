92b4fb58f665ac63b856918043c1687ec9ba1d88
Jeroen Gordijn <jeroen.gordijn@gmail.com>
2017-01-27 13:48:09 +0100

    add plugin

--- startElk.sh
+++ startElk.sh
@@ -11,3 +11,4 @@ docker run -d -p 9200:9200 -p 9300:9300 -p 5601:5601 -p 5000:5000 -p 5044:5044 \
 ELK_DOCKER_HASH=`docker ps |grep elk | awk '{print $1}'`
 docker exec -ti $ELK_DOCKER_HASH cp -R /var/lib/elasticsearch/backups-original /var/lib/elasticsearch/backups
 docker exec -ti $ELK_DOCKER_HASH chown -R elasticsearch:elasticsearch /var/lib/elasticsearch/backups
+docker exec -ti $ELK_DOCKER_HASH /opt/kibana/bin/kibana-plugin install https://github.com/prelert/kibana-swimlane-vis/releases/download/v5.0.2/prelert_swimlane_vis-5.0.2.zip
